bf074b04e5d4d19740c187e4023d413ac55ea1a9
Kwok-kuen Cheung <cheungpat@oursky.com>
2016-10-26 14:03:07 +0800

    Clear skydb tests

--- pkg/server/skydb/db_test.go
+++ pkg/server/skydb/db_test.go
@@ -18,45 +18,5 @@ import (
 	"testing"
 )
 
-type fakeConn struct {
-	Conn
-	AppName      string
-	AccessModel  AccessModel
-	OptionString string
-}
-
-type fakeDriver struct {
-	Driver
-}
-
-func (driver fakeDriver) Open(appName string, accessModel AccessModel, optionString string, migrate bool) (Conn, error) {
-	return fakeConn{
-		AppName:      appName,
-		AccessModel:  accessModel,
-		OptionString: optionString,
-	}, nil
-}
-
 func TestOpen(t *testing.T) {
-	defer unregisterAllDrivers()
-
-	Register("fakeImpl", fakeDriver{})
-
-	if driver, err := Open("fakeImpl", "com.example.app.test", "role", "fakeOption", true); err != nil {
-		t.Fatalf("got err: %v, want a driver", err.Error())
-	} else {
-		if driver, ok := driver.(fakeConn); !ok {
-			t.Fatalf("got driver = %v, want a driver of type fakeDriver", driver)
-		} else {
-			if driver.AppName != "com.example.app.test" {
-				t.Fatalf("got driver.AppName = %v, want \"com.example.app.test\"", driver.AppName)
-			}
-			if driver.AccessModel != RoleBasedAccess {
-				t.Fatalf("got driver.AccessModel = %v, want \"RoleBasedAccess\"", driver.AccessModel)
-			}
-			if driver.OptionString != "fakeOption" {
-				t.Fatalf("got driver.OptionString = %v, want \"fakeOption\"", driver.OptionString)
-			}
-		}
-	}
 }
