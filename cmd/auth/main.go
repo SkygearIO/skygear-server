c4b45d36f6a7737ae8fc600ceb9e4fc16281cd64
Steven Chan <stevenchan@oursky.com>
2018-09-13 19:04:57 +0800

    Put default AuthContextResolver in default folder

--- cmd/auth/main.go
+++ cmd/auth/main.go
@@ -9,7 +9,7 @@ import (
 	"time"
 
 	"github.com/skygeario/skygear-server/pkg/core/auth/authinfo"
-	"github.com/skygeario/skygear-server/pkg/core/auth/authn"
+	defaultAuthn "github.com/skygeario/skygear-server/pkg/core/auth/authn/default"
 	"github.com/skygeario/skygear-server/pkg/core/auth/authtoken"
 
 	"github.com/kelseyhightower/envconfig"
@@ -51,7 +51,7 @@ func main() {
 	}
 
 	srv.SetAuthContextResolverFactory(
-		authn.DefaultAuthContextResolverFactory{ProviderGraph: authDependency},
+		defaultAuthn.AuthContextResolverFactory{ProviderGraph: authDependency},
 	)
 
 	handler.AttachLoginHandler(&srv, authDependency)
