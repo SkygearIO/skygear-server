cdd9a84182add3f53608a43e4fda293b2b724818
Carmen <carmen@oursky.com>
2019-09-18 15:16:44 +0800

    Support connecting redis sentinel for session store

--- pkg/core/redis/pool.go
+++ pkg/core/redis/pool.go
@@ -1,20 +1,33 @@
 package redis
 
 import (
+	"errors"
 	"fmt"
 	"time"
 
+	"github.com/FZambia/sentinel"
 	"github.com/gomodule/redigo/redis"
 )
 
+// When sentinel is enabled, host and port will be ignored
 type Configuration struct {
-	Host     string `envconfig:"HOST"`
-	Port     int    `envconfig:"PORT" default:"6379"`
-	Password string `envconfig:"PASSWORD"`
-	DB       int    `envconfig:"DB" default:"0"`
+	SentinelEnabled bool           `envconfig:"SENTINEL_ENABLED"`
+	Host            string         `envconfig:"HOST"`
+	Port            int            `envconfig:"PORT" default:"6379"`
+	Password        string         `envconfig:"PASSWORD"`
+	DB              int            `envconfig:"DB" default:"0"`
+	Sentinel        SentinelConfig `envconfig:"SENTINEL"`
+}
+
+type SentinelConfig struct {
+	Addrs      []string `envconfig:"ADDRS"`
+	MasterName string   `envconfig:"MASTER_NAME" default:"mymaster"`
 }
 
 func NewPool(config Configuration) *redis.Pool {
+	if config.SentinelEnabled {
+		return newSentinelPool(config)
+	}
 	hostPort := fmt.Sprintf("%s:%d", config.Host, config.Port)
 	// TODO(pool): configurable / profile for good value?
 	return &redis.Pool{
@@ -32,6 +45,55 @@ func NewPool(config Configuration) *redis.Pool {
 		TestOnBorrow: func(conn redis.Conn, t time.Time) (err error) {
 			_, err = conn.Do("PING")
 			return
+		},
+	}
+}
+
+func newSentinelPool(config Configuration) *redis.Pool {
+	sntnl := &sentinel.Sentinel{
+		Addrs:      config.Sentinel.Addrs,
+		MasterName: config.Sentinel.MasterName,
+		Dial: func(addr string) (redis.Conn, error) {
+			timeout := 500 * time.Millisecond
+			c, err := redis.Dial(
+				"tcp",
+				addr,
+				redis.DialConnectTimeout(timeout),
+				redis.DialReadTimeout(timeout),
+				redis.DialWriteTimeout(timeout),
+			)
+			if err != nil {
+				return nil, err
+			}
+			return c, nil
+		},
+	}
+	return &redis.Pool{
+		MaxIdle:     3,
+		MaxActive:   64,
+		Wait:        true,
+		IdleTimeout: 240 * time.Second,
+		Dial: func() (redis.Conn, error) {
+			masterAddr, err := sntnl.MasterAddr()
+			if err != nil {
+				return nil, err
+			}
+			c, err := redis.Dial(
+				"tcp",
+				masterAddr,
+				redis.DialDatabase(config.DB),
+				redis.DialPassword(config.Password),
+			)
+			if err != nil {
+				return nil, err
+			}
+			return c, nil
+		},
+		TestOnBorrow: func(c redis.Conn, t time.Time) error {
+			if !sentinel.TestRole(c, "master") {
+				return errors.New("Role check failed")
+			}
+			return nil
 		},
 	}
 }
